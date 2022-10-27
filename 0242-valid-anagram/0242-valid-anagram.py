dd310692103b8c70763dfaa11dc6af85e34fda95
ogudal20 <omar_gudal@hotmail.co.uk>
2022-10-27 14:14:54 +0100

    Time: 139 ms (13.86%), Space: 14.4 MB (67.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,13 +3,13 @@ class Solution:
         countS = {}
         countT = {}
         
+        # if not the same length then can't be anagram.
         if len(s) != len(t):
             return False
         
+        # then we check the number of characters in both strings if the same then its as anagram
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
             
         return countS == countT
-            
-        
