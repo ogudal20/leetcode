dd3c3090abcd69c6c4d3f33b6de5025f9ea821ab
ogudal20 <omar_gudal@hotmail.co.uk>
2022-10-27 19:57:17 +0100

    Time: 259 ms (25.91%), Space: 19.6 MB (26.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,19 +3,16 @@ class Solution:
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
         
-        # get count of each number
         for n in nums:
             count[n] = 1 + count.get(n, 0)
             
-        # add the count of each number in the list.
         for i, c in count.items():
             freq[c].append(i)
             
-        # Check if top k results.
         result = []
         for i in range(len(nums), 0, -1):
             for j in freq[i]:
                 result.append(j)
                 if len(result) == k:
                     return result
-            
+                
