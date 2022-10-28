e3de828e9d5e06242961405a596179f81f4df8b9
ogudal20 <omar_gudal@hotmail.co.uk>
2022-10-28 13:14:41 +0100

    Time: 114 ms (88.76%), Space: 19.8 MB (23.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,6 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         result = defaultdict(list)
         
+        # Array to store the count of letter in strs.
         for s in strs:
             count = [0] * 26
             
