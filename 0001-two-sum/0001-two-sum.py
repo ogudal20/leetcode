08d9fab28e89eca8360ebf37d43218980dbed764
ogudal20 <omar_gudal@hotmail.co.uk>
2022-12-02 21:52:12 +0000

    Time: 144 ms (43.86%), Space: 15.2 MB (54.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMatch = {}
-        
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMatch:
-                return [prevMatch[diff], i]
-            prevMatch[n] = i
+          # Create a hash table to store the numbers in the array
+        table = {}
+
+    # Iterate over the numbers in the array
+        for i, num in enumerate(nums):
+        # Check if the difference between the target and the current number exists in the hash table
+            if target - num in table:
+            # If it does, we have found the two numbers that add up to the target,
+            # so we can return their indices
+                return [table[target - num], i]
+
+        # If the difference does not exist in the hash table, add the current number to the table
+            table[num] = i
+
+    # If we reach this point, we have not found the two numbers that add up to the target,
+    # so we return an empty list
+        return []
